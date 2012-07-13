ddfcdf24e798f64458fb1173dec8e0a082600ac5
Durran Jordan <durran@gmail.com>
2012-07-13 15:39:34 +0200

    More flusing out of individual type wrapping via string

--- src/main/scala/org/mongoid/scooter/bson/StringWrapper.scala
+++ src/main/scala/org/mongoid/scooter/bson/StringWrapper.scala
@@ -2,12 +2,43 @@ package org.mongoid.scooter.bson
 
 import java.nio.ByteBuffer
 
+/**
+ * Companion object for the StringWrapper class.
+ */
 object StringWrapper {
 
+  /**
+   * Implicit conversion from a String to a StringWrapper.
+   *
+   * @param target The String that is getting wrapped.
+   * @return The StringWrapper around the String.
+   */
   implicit def wrap(target: String) : StringWrapper = new StringWrapper(target)
 }
 
+/**
+ * Wraps strings to provide additional behaviour around BSON serialization.
+ */
 class StringWrapper(target: String) extends Serializable {
 
-  def bsonDump(buffer: ByteBuffer, key: String) = {}
+  /**
+   * See: http://bsonspec.org/#/specification
+   *
+   * @return The byte representation of a UTF-8 string.
+   */
+  final val TYPE : Byte = 2
+
+  /**
+   * Dump the string to the buffer in it's proper BSON format.
+   *
+   * @param buffer The buffer being written to.
+   * @param key The string key to this instance string value.
+   */
+  def bsonDump(buffer: ByteBuffer, key: String) = {
+    buffer.
+      put(TYPE).
+      putInt(target.length).
+      put(target.getBytes).
+      putInt(0)
+  }
 }
--- src/test/scala/org/mongoid/scooter/bson/StringWrapperSpec.scala
+++ src/test/scala/org/mongoid/scooter/bson/StringWrapperSpec.scala
@@ -3,16 +3,24 @@ import org.scalatest.matchers.MustMatchers
 
 import org.mongoid.scooter.bson.StringWrapper
 
+import java.nio.{ ByteBuffer, ByteOrder }
+
 class StringWrapperSpec extends FunSpec with MustMatchers {
 
   describe("org.mongoid.scooter.bson.StringWrapper") {
 
+    val buffer = ByteBuffer.allocate(24).order(ByteOrder.LITTLE_ENDIAN)
+
     describe("#bsonDump") {
 
-      val string = "scooter"
+      val key = "hello"
+      val value = "world"
+
+      val wrapper = new StringWrapper("world")
 
       it("serializes the string to the buffer") {
-
+        wrapper.bsonDump(buffer, key)
+        buffer.array.foreach(arg => println(arg))
       }
     }
   }
