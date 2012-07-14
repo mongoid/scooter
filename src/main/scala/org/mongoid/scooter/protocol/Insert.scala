b2d552e9666dd4d984b8001a5d322c4d60ef7454
Durran Jordan <durran@gmail.com>
2012-07-14 16:58:37 +0200

    Add scaladoc to Insert

--- src/main/scala/org/mongoid/scooter/protocol/Insert.scala
+++ src/main/scala/org/mongoid/scooter/protocol/Insert.scala
@@ -4,13 +4,33 @@ import org.mongoid.scooter.Collection
 
 import java.nio.ByteBuffer
 
+/**
+ * Companion object for the Insert class.
+ */
 object Insert {
 
+  /**
+   * Instantiate a new Insert message.
+   *
+   * @param collection The Collection to insert into.
+   * @param documents The documents to insert.
+   *
+   * @return The Insert message.
+   */
   def apply(collection: Collection, documents: Array[Map[String, Any]]) = {
     new Insert(collection.fullName, documents)
   }
 }
 
+/**
+ * This class encapsulates behaviour for the OP_INSERT command in the
+ * MongoDB Wire Protocol.
+ *
+ * See: http://www.mongodb.org/display/DOCS/Mongo+Wire+Protocol
+ *
+ * @param name The full name of the Collection.
+ * @param documents The documents to insert.
+ */
 class Insert(name: String, documents: Array[Map[String, Any]]) extends Message {
 
   def serialize(buffer: ByteBuffer) = {}
