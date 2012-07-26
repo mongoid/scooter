88a229a6cc7352514d64fc35c9714ee50af68aed
Durran Jordan <durran@gmail.com>
2012-07-26 18:33:12 +0200

    Convert protocol insert spec to specs2

--- src/test/scala/org/mongoid/scooter/protocol/InsertSpec.scala
+++ src/test/scala/org/mongoid/scooter/protocol/InsertSpec.scala
@@ -1,44 +1,37 @@
 import org.mongoid.scooter.bson.MutableBuffer
 import org.mongoid.scooter.protocol.Insert
-
-import org.scalatest.FunSpec
-import org.scalatest.matchers.MustMatchers
-
+import org.specs2.mutable.Specification
 import scala.collection.immutable.HashMap
 
-class InsertSpec extends FunSpec with MustMatchers {
+class InsertSpec extends Specification {
 
-  describe("Insert") {
+  val doc = HashMap("hi" -> "ya")
+  val documents = Array(doc)
+  val insert = new Insert("scooter_test.users", documents)
 
-    val document = HashMap("hi" -> "ya")
-    val documents = Array(document)
-    val insert = new Insert("scooter_test.users", documents)
+  "Insert#serialize" should {
 
-    describe("#serialize") {
+    val header = Array(55, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, -46, 7, 0, 0)
+    val flags = Array(0, 0, 0, 0);
+    val name = Array(115, 99, 111, 111, 116, 101, 114, 95, 116, 101, 115, 116, 46, 117, 115, 101, 114, 115, 0)
+    val document = Array(16, 0, 0, 0, 2, 104, 105, 0, 3, 0, 0, 0, 121, 97, 0, 0)
+    val buffer = MutableBuffer(55)
 
-      val header   = Array(55, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, -46, 7, 0, 0)
-      val flags    = Array(0, 0, 0, 0);
-      val name     = Array(115, 99, 111, 111, 116, 101, 114, 95, 116, 101, 115, 116, 46, 117, 115, 101, 114, 115, 0)
-      val document = Array(16, 0, 0, 0, 2, 104, 105, 0, 3, 0, 0, 0, 121, 97, 0, 0)
-
-      val buffer = MutableBuffer(55)
-
-      it("adds the document to the buffer") {
-        val bytes = header ++ flags ++ name ++ document
-        insert.serialize(buffer)
-        buffer.array must be(bytes)
-      }
+    "add the document to the buffer" in {
+      val bytes = header ++ flags ++ name ++ document
+      insert.serialize(buffer)
+      buffer.array must beEqualTo(bytes)
     }
+  }
 
-    describe("#serializeHeader") {
+  "Insert#serializeHeader" should {
 
-      val bytes = Array(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, -46, 7, 0, 0)
-      val buffer = MutableBuffer(16)
+    val bytes = Array(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, -46, 7, 0, 0)
+    val buffer = MutableBuffer(16)
 
-      it("adds the header as 32 bit integers") {
-        insert.serializeHeader(buffer)
-        buffer.array must be(bytes)
-      }
+    "add the header as 32 bit integers" in {
+      insert.serializeHeader(buffer)
+      buffer.array must beEqualTo(bytes)
     }
   }
 }
