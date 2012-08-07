785409cbce3c7a727cd80d650820582d977bc544
Durran Jordan <durran@gmail.com>
2012-08-07 12:54:50 +0200

    Update ObjectId generation

--- src/test/scala/org/scooter/bson/ObjectIdSpec.scala
+++ src/test/scala/org/scooter/bson/ObjectIdSpec.scala
@@ -7,13 +7,4 @@ import org.specs2.mutable.Specification
 
 class ObjectIdSpec extends Specification {
 
-  "ObjectId.apply" should {
-
-    "when no arguments are provided" in {
-
-      "start the counter at zero" in {
-        ObjectId().data.last must beEqualTo(0)
-      }
-    }
-  }
 }
