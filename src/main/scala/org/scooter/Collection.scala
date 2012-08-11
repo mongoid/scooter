55be4aa328ab58e177e7fa39fe58e3a508899374
Durran Jordan <durran@gmail.com>
2012-08-11 18:31:46 +0200

    Add find by a document

--- src/main/scala/org/scooter/Collection.scala
+++ src/main/scala/org/scooter/Collection.scala
@@ -15,12 +15,31 @@ class Collection(database: Database, name: String, session: Session) {
   /**
    * Get all documents that are in the Collection.
    *
+   * @example Find all documents.
+   *  session.users.find
+   *
    * @return The chainable Query object for all documents.
    */
   def find = Query(this, session)
 
   /**
    * Get all documents matching the selector in the Collection.
+   *
+   * @example Find matching documents by the provided Document.
+   *  val document = Document("firstName" -> "Sid")
+   *  session.users.find(document)
+   *
+   * @param selector The MongoDB selector as a Document.
+   *
+   * @return The chainable Query object for the matching documents.
+   */
+  def find(selector: Document) = Query(this, selector, session)
+
+  /**
+   * Get all documents matching the selector in the Collection.
+   *
+   * @example Find mtching documents for the selector.
+   *  session.users.find("firstName" -> "Sid")
    *
    * @param selector The MongoDB selector in Dumpable form.
    *
