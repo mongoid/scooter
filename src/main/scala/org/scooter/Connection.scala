a5d75943dacf8fddc7833731c6fbe0350acae5c1
Durran Jordan <durran@gmail.com>
2012-08-14 22:30:59 +0200

    Refactor bootstrap code into its own module

--- src/main/scala/org/scooter/Connection.scala
+++ src/main/scala/org/scooter/Connection.scala
@@ -1,16 +1,10 @@
 package org.scooter
 
 import java.net.SocketAddress
-import java.nio.ByteOrder._
-import java.util.concurrent.{ Executors => Ex }
 
-import org.jboss.netty.bootstrap.{ ClientBootstrap => Bootstrap }
-import org.jboss.netty.buffer.{ HeapChannelBufferFactory => BufferFactory }
 import org.jboss.netty.channel.Channel
-import org.jboss.netty.channel.socket.nio.{ NioClientSocketChannelFactory => Factory }
 
-import org.scooter.functional.Utilities._
-import org.scooter.io.{ Handler, Pipeline }
+import org.scooter.io.{ Bootstrap, Handler, Pipeline }
 import org.scooter.protocol.{ Command, Request }
 
 /**
@@ -26,45 +20,7 @@ object Connection {
    * @return The new Connection.
    */
   protected[scooter] def apply(address: SocketAddress) = {
-    new Connection(future(address).awaitUninterruptibly.getChannel)
-  }
-
-  /**
-   * Get the ClientBootstrap to use with the connection.
-   *
-   * @note This sets some default options for the bootstrap:
-   *  - The default byte order should always be little endian.
-   *  - Use TCP_NODELAY at all times.
-   *  - Set the pipeline factory to our pipeline.
-   *
-   * @return The ClientBootstrap.
-   */
-  private def bootstrap = {
-    new Bootstrap(factory).tap(
-      boot => {
-        boot.setOption("bufferFactory", new BufferFactory(LITTLE_ENDIAN))
-        boot.setOption("tcpNoDelay", true)
-        boot.setPipelineFactory(new Pipeline)
-      }
-    )
-  }
-
-  /**
-   * Get the ChannelFuture for the provided address.
-   *
-   * @param address The SocketAddress.
-   *
-   * @return The ChannelFuture.
-   */
-  private def future(address: SocketAddress) = bootstrap.connect(address)
-
-  /**
-   * Get the NioClientSocketChannelFactory to use in creating the bootstrap.
-   *
-   * @return The new NioClientSocketChannelFactory.
-   */
-  private def factory = {
-    new Factory(Ex.newCachedThreadPool, Ex.newCachedThreadPool)
+    new Connection(Bootstrap.channel(address))
   }
 }
 
---
+++ src/main/scala/org/scooter/io/Bootstrap.scala
@@ -0,0 +1,71 @@
+package org.scooter.io
+
+import java.net.SocketAddress
+import java.nio.ByteOrder._
+import java.util.concurrent.{ Executors => Ex }
+
+import org.jboss.netty.bootstrap.ClientBootstrap
+import org.jboss.netty.buffer.{ HeapChannelBufferFactory => BufferFactory }
+import org.jboss.netty.channel.socket.nio.{ NioClientSocketChannelFactory => Factory }
+
+import org.scooter.functional.Utilities._
+
+/**
+ * The singleton bootstrap for Scooter connections.
+ */
+object Bootstrap {
+
+  /**
+   * Get the Channel for this bootstrap.
+   *
+   * @param address The SocketAddress to connect to.
+   *
+   * @return The Channel.
+   */
+  def channel(address: SocketAddress) = {
+    future(address).awaitUninterruptibly.getChannel
+  }
+
+  /**
+   * Get the ClientBootstrap to use with the connection.
+   *
+   * @note This sets some default options for the bootstrap:
+   *  - The default byte order should always be little endian.
+   *  - Use TCP_NODELAY at all times.
+   *  - Set the pipeline factory to our pipeline.
+   *
+   * @return The ClientBootstrap.
+   */
+  private def bootstrap = {
+    new ClientBootstrap(factory).tap(boot => configureBootstrap(boot))
+  }
+
+  /**
+   * Configure the provided boostrap with the default options.
+   *
+   * @param bootstrap The ClientBootstrap.
+   */
+  private def configureBootstrap(bootstrap: ClientBootstrap) = {
+    bootstrap.setOption("bufferFactory", new BufferFactory(LITTLE_ENDIAN))
+    bootstrap.setOption("tcpNoDelay", true)
+    bootstrap.setPipelineFactory(new Pipeline)
+  }
+
+  /**
+   * Get the NioClientSocketChannelFactory to use in creating the bootstrap.
+   *
+   * @return The new NioClientSocketChannelFactory.
+   */
+  private def factory = {
+    new Factory(Ex.newCachedThreadPool, Ex.newCachedThreadPool)
+  }
+
+  /**
+   * Get the ChannelFuture for the provided address.
+   *
+   * @param address The SocketAddress.
+   *
+   * @return The ChannelFuture.
+   */
+  private def future(address: SocketAddress) = bootstrap.connect(address)
+}
